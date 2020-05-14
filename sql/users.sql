bba13bdbf1d014f2f40f01c6db1390aafc671086
Nirmalya Ghosh <nirmalya.gh24@gmail.com>
2020-05-14 13:58:46 -0700

    Added USER table SQL creation query

---
+++ sql/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+( id SERIAL PRIMARY KRY,
+email VARCHAR(100) UNIQUE NOT NULL,
+first_name VARCHAR(100) NOT NULL,
+last_name VARCHAR(100) NOT NULL,
+password VARCHAR(100) NOT NULL,
+is_admin BOOLEAN DEFAULT(false),
+created_on DATE NOT NULL);
