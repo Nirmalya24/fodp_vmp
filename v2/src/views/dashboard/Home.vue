69edec92e98586778aaeb274428cd5f6cc3a714f
Kyle Roland <kcroland@uw.edu>
2020-05-19 17:08:16 -0400

    Add new dashboard

---
+++ v2/src/views/dashboard/Home.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="display-2 pa-3">Home</h1>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
---
+++ v2/src/views/dashboard/Tables.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1 class="display-2 pa-3">Tables</h1>
+</template>
+
+<script>
+export default {
+  name: 'Tables'
+}
+</script>
